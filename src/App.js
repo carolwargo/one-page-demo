428f62026011bf54eb1954706b49a4f1c5dd5715
Carol Wargo <Carolwargo@gmail.com>
2024-06-08 13:16:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
 
 import HomePage from "./pages/HomePage";
 import ErrorBoundary from "./components/ErrorBoundary";
