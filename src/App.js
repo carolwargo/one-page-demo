31ffdda9da5d7b0965a1d9d6dbda5e9892694b7b
Carol Wargo <Carolwargo@gmail.com>
2024-06-08 12:30:26 -0400

    installs

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter basename="/one-page">
+      <ErrorBoundary>
         <Routes>
           <Route path="/" element={<HomePage />} />
           {/* Add a catch-all route for handling errors */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <ErrorBoundary />
+        </ErrorBoundary>
       </BrowserRouter>
     </div>
   );
@@ -25,5 +26,6 @@ function NotFound() {
   console.error("Page not found!"); // Log error to console
   return <h1>404 - Not Found</h1>;
 }
+
 
 export default App;
